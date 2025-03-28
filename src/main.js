ad847ba730b8f084edd3c8c2969bf0b4dd466681
ysmk <0620komako@gmail.com>
2025-03-28 20:13:13 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,35 @@
+// モジュールをインポート
+import { fetchHistoryData } from "../my-modules/fetchHistoryData.js";
+import { submitData } from "../my-modules/submit-data.js";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Cloud Firestoreの初期化
+const db = getFirestore(app);
+
+// モジュールを使用
+if (document.getElementById("js-history")) {
+  fetchHistoryData(getDocs, collection, db);
+}
+
+if (document.getElementById("js-form")) {
+  document
+    .getElementById("js-form")
+    .addEventListener("submit", (e) => submitData(e, addDoc, collection, db));
+}
